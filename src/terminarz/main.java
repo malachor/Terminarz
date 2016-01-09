d26fc67557fa212765981c31f02fa114402e84f3
malachor <bodzioxdd3@gmail.com>
2016-01-09 20:42:41 +0100

    testuj

--- src/terminarz/main.java
+++ src/terminarz/main.java
@@ -4,6 +4,11 @@ public class main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		// TODO Auto-generated method stub
+		// TODO Auto-generated method stub
+		// TODO Auto-generated method stub
+		// TODO Auto-generated method stub
+		System.out.print("helow");
 
 	}
 
